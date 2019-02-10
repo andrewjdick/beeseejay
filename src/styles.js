81a5f9a668447d0ed3d15bab756d0b0fb8c9a16b
Andrew James Dick <andrewdick1987@gmail.com>
2019-02-10 12:55:38 +0000

    🎨 hide html overflow

--- src/styles.js
+++ src/styles.js
@@ -19,7 +19,8 @@ export const GlobalStyles = createGlobalStyle`
     display: flex;
     flex-direction: column;
     width: 100%;
-    min-height: 100%;
+    height: 100%;
+    overflow: hidden;
   }
   
   body, div#root {
